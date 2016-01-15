fe40fbf9cfad1fd2e2716da226b3c78cf5044d51
Rachel Cartwright <Rachel.D.Cartwright@gmail.com>
2016-01-14 21:48:26 -0700

    make it work

--- gulpfile.js
+++ gulpfile.js
@@ -15,10 +15,10 @@ var config = {
 	paths: {
 		html: './src/*.html',
 		js: './src/**/*.js',
-		/*css: [
+		css: [
       		'node_modules/bootstrap/dist/css/bootstrap.min.css',
       		'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
-    	],*/
+    	],
 		dist: './dist',
 		mainJs: './src/main.js'
 	}
@@ -55,11 +55,11 @@ gulp.task('js', function() {
 		.pipe(connect.reload());
 });
 
-/*gulp.task('css', function() {
+gulp.task('css', function() {
 	gulp.src(config.paths.css)
 		.pipe(concat('bundle.css'))
 		.pipe(gulp.dest(config.paths.dist + '/css'));
-});*/
+});
 
 gulp.task('lint', function() {
 	return gulp.src(config.paths.js)
@@ -72,4 +72,4 @@ gulp.task('watch', function() {
 	gulp.watch(config.paths.js, ['js', 'lint']);
 });
 
-gulp.task('default', ['html', 'lint', 'js', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
--- src/main.js
+++ src/main.js
@@ -3,3 +3,4 @@ $ = jQuery = require('jquery');
 var App = console.log('Hello world from Browserify');
 
 module.exports = App;
+
