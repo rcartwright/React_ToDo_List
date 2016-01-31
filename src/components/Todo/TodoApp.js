7e88f2493c4d2ec9e5b9858d43a1df052535d574
Rachel Cartwright <Rachel.D.Cartwright@gmail.com>
2016-01-30 19:40:21 -0700

    add flux and make show correctly

--- src/components/Todo/TodoApp.js
+++ src/components/Todo/TodoApp.js
@@ -2,28 +2,43 @@ var React = require('react');
 var TodoBanner = require('./TodoBanner/TodoBanner');
 var TodoForm = require('./TodoForm/TodoForm');
 var TodoList = require('./TodoList/TodoList');
+var todoActions = require('../../actions/TodoActions');
+var todoStore = require('../../stores/TodoStore');
 
     var TodoApp = React.createClass({
       getInitialState: function() {
-      return {items: ['Todo item #1', 'Todo item #2']};
+      return {
+        list: (['Todo item #1', 'Todo item #2'], todoStore.getList())
+        }
       },
-      updateItems: function(newItem) {
-        var allItems = this.state.items.concat([newItem]);
+      componentDidMount: function(){
+        todoStore.addChangeListener(this._onChange);
+      },
+      componentWillUnmount: function(){
+        todoStore.removeChangeListener(this._onChange);
+      },
+      handleAddItem: function(newItem){
+        todoActions.addItem(newItem);
+      },
+      handleRemoveItem: function(index){
+        todoActions.removeItem(index);
+      },
+      _onChange: function(){
         this.setState({
-        items: allItems
-      });
+          //was list
+          list: todoStore.getList()
+        })
       },
       render: function() {
-      return (
-        <div className="app-container">
-        <TodoBanner/>
-        <TodoList items={this.state.items}/>
-        <TodoForm onFormSubmit={this.updateItems}/>
-        </div>
-      );
-      }
+          return (
+            <div className="app-container">
+            <TodoBanner/>  //this.state.list or
+            <TodoList items={this.state.list} remove={this.handleRemoveItem}/>
+            <TodoForm add={this.handleAddItem}/>
+            </div>
+          );
+        }
     });
-
 
 
     module.exports = TodoApp;
