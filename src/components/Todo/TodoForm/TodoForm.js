92a07ce9075abfa89c8250e0acec7137627e4a31
Rachel D. Cartwright <Rachel.D.Cartwright@gmail.com>
2016-05-21 21:48:18 -0700

    change to new ReactDOM

--- src/components/Todo/TodoForm/TodoForm.js
+++ src/components/Todo/TodoForm/TodoForm.js
@@ -1,23 +1,23 @@
 "use strict";
 
 var React = require('react');
+var ReactDOM = require('react-dom');
 
-
-  var TodoForm = React.createClass({
-    handleSubmit: function(e){
-    if(e.keyCode === 13){
-      var newItem = this.refs.newItem.getDOMNode().value;
-      this.refs.newItem.getDOMNode().value = '';
-      this.props.add(newItem);
+var TodoForm = React.createClass({
+  handleSubmit: function(e){
+  if(e.keyCode === 13){
+    var newItem = ReactDOM.findDOMNode(this.refs.newItem).value;
+    ReactDOM.findDOMNode(this.refs.newItem).value = '';
+    this.props.add(newItem);
+  }
+},
+render: function(){
+  return (
+    <div>
+      <input type="text" ref="newItem" className="form-control" placeholder="New Item" onKeyDown={this.handleSubmit} />
+    </div>
+      );
     }
-  },
-      render: function(){
-        return (
-        <div>
-        <input type="text" ref="newItem" className="form-control" placeholder="New Item" onKeyDown={this.handleSubmit} />
-      </div>
-        );
-      }
-    });
+  });
 
 module.exports = TodoForm;
