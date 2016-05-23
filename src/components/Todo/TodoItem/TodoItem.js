a9fc378cfcf71d2940a292d37bd4af233da9e60f
Rachel D. Cartwright <Rachel.D.Cartwright@gmail.com>
2016-05-22 22:47:39 -0700

    make remove item work

--- src/components/Todo/TodoItem/TodoItem.js
+++ src/components/Todo/TodoItem/TodoItem.js
@@ -1,11 +1,15 @@
 "use strict";
 
 var React = require('react');
-
+var ReactDOM = require('react-dom');
 
 var TodoListItem = React.createClass({
   render: function() {
-    return <li>{this.props.data}</li>;
+    return (
+    	<li id={this.props.id} ref={this.props.data}>
+    		{this.props.data} <button onClick={this.props.onClick}>X</button>
+    	</li>
+    );
   }
 });
 
