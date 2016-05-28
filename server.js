65f0326ae88884116055a3a9b1beaa8e0d45a48d
Rachel D. Cartwright <Rachel.D.Cartwright@gmail.com>
2016-05-28 14:18:45 -0700

    change to browserHistory and make urls work correctly

--- server.js
+++ server.js
@@ -1,41 +1,16 @@
 var express = require('express');
+var path = require('path');
+var port = process.env.PORT || 8080;
 var app = express();
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
+// serve static assets normally
 app.use(express.static(__dirname + '/dist'));
 
-// set the home page route
-app.get('/', function(req, res) {
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+})
 
-    // ejs render automatically looks in the views folder
-    res.render('index');
- 
-// return app;
-});
-
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
-
-/*var path = require('path')
-var express = require('express')
-
-module.exports = {
-  app: function () {
-    var app = express()
-    var indexPath = path.join(__dirname, '/../index.html')
-    var publicPath = express.static(path.join(__dirname, '../public'))
-
-    app.use('/public', publicPath)
-    app.get('/', function (_, res) { res.sendFile(indexPath) })
-
-    return app
-  }
-}*/
+app.listen(port);
+console.log("server started on port " + port);
